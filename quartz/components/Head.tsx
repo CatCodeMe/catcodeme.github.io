5454c97ea35713093e74f8257d7211cdf959ba94
hulinjiang <hulinjiang@58.com>
2024-03-09 16:35:08 +0800

    feat: og优化,添加字体css

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -18,33 +18,39 @@ export default (() => {
     const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
-      <head>
-        <title>{title}</title>
-        <meta charSet="utf-8" />
-        {cfg.theme.cdnCaching && (
-          <>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-          </>
-        )}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
-        <meta property="og:width" content="1200" />
-        <meta property="og:height" content="675" />
-        <link rel="icon" href={iconPath} />
-        <meta name="description" content={description} />
-        <meta name="generator" content="Quartz" />
-        {css.map((href) => (
-          <link key={href} href={href} rel="stylesheet" type="text/css" spa-preserve />
-        ))}
-        {js
-          .filter((resource) => resource.loadTime === "beforeDOMReady")
-          .map((res) => JSResourceToScriptElement(res, true))}
-      </head>
+        <head>
+            <title>{title}</title>
+            <meta charSet="utf-8"/>
+            {cfg.theme.cdnCaching && (
+                <>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                </>
+            )}
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta property="og:title" content={title}/>
+            <meta property="og:type" content="article"/>
+            <meta property="og:url" content={path}/>
+            <meta property="og:description" content={description}/>
+            {cfg.baseUrl && <meta property="og:image" content={ogImagePath}/>}
+            <meta property="og:width" content="1200"/>
+            <meta property="og:height" content="675"/>
+            <link rel="icon" href={iconPath}/>
+            <link rel="stylesheet"
+                  href="https://cdn.bootcdn.net/ajax/libs/lxgw-wenkai-screen-webfont/1.7.0/style.min.css" spa-preserve/>
+            <link rel="stylesheet"
+                  href="https://cdn.jsdelivr.net/npm/firacode@6.2.0/distr/fira_code.css" spa-preserve/>
+            <meta name="description" content={description}/>
+            <meta name="generator" content="Quartz"/>
+            {css.map((href) => (
+                <link key={href} href={href} rel="stylesheet" type="text/css" spa-preserve/>
+            ))}
+            {js
+                .filter((resource) => resource.loadTime === "beforeDOMReady")
+                .map((res) => JSResourceToScriptElement(res, true))}
+        </head>
     )
   }
 
-  return Head
+    return Head
 }) satisfies QuartzComponentConstructor
