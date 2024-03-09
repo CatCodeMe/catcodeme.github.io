0967e6a179fbd9d02d5561860cbb0460425c857d
hulinjiang <hulinjiang@58.com>
2024-03-09 16:34:30 +0800

    feat: 调整breadcrumb为/

--- quartz/components/Breadcrumbs.tsx
+++ quartz/components/Breadcrumbs.tsx
@@ -33,7 +33,7 @@ interface BreadcrumbOptions {
 }
 
 const defaultOptions: BreadcrumbOptions = {
-  spacerSymbol: "❯",
+  spacerSymbol: "/",
   rootName: "Home",
   resolveFrontmatterTitle: true,
   hideOnRoot: true,
