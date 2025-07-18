0d8f82157a765891ebd7786c29b32f15c71eb5c9
catcodeme <1020082805@qq.com>
2025-07-18 13:32:39 +0800

    update repo

--- src/scripts/fetch-comments.js
+++ src/scripts/fetch-comments.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 const GITHUB_TOKEN = process.env.gh_action_token_PAT
 const DISCUSSION_CATEGORY_NAME = 'General';
 const REPO_OWNER = 'catcodeme';
-const REPO_NAME = 'github-discussion';
+const REPO_NAME = 'catcodeme.github.io';
 
 async function fetchDiscussions() {
   const response = await fetch('https://api.github.com/graphql', {
