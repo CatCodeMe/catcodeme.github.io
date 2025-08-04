346920acc088070b5f91a23ee87e66790a79c1e6
catcodeme <1020082805@qq.com>
2025-08-04 17:48:57 +0800

    fix pem issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -206,10 +206,10 @@ export default defineConfig({
     ],
     server: {
       host: true,
-      https: {
-        key: fs.readFileSync('./localhost+2-key.pem'),
-        cert: fs.readFileSync('./localhost+2.pem')
-      }
+      // https: {
+      //   key: fs.readFileSync('./localhost+2-key.pem'),
+      //   cert: fs.readFileSync('./localhost+2.pem')
+      // }
     }
   }
 })
