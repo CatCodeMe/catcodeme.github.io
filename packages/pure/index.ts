23fdc2bc1b955debd4ee06b92b1715fce853fc39
catcodeme <1020082805@qq.com>
2025-08-07 17:46:01 +0800

    修复 expressive-code 折叠效果和 details 样式冲突问题

--- packages/pure/index.ts
+++ packages/pure/index.ts
@@ -6,10 +6,10 @@ import type { AstroIntegration, RehypePlugins, RemarkPlugins } from 'astro'
 // Integrations
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import rehypeExternalLinks from 'rehype-external-links'
 import UnoCSS from 'unocss/astro'
-
-import rehypeExternalLinks from './plugins/rehype-external-links'
 import rehypeTable from './plugins/rehype-table'
+
 import { remarkAddZoomable, remarkReadingTime } from './plugins/remark-plugins'
 import { vitePluginUserConfig } from './plugins/virtual-user-config'
 import { UserConfigSchema, type UserInputConfig } from './types/user-config'
