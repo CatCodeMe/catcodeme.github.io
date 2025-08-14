57dcd897b904c78a258911b9f3b1bd073faa6e29
catcodeme <1020082805@qq.com>
2025-08-14 11:34:02 +0800

    修复 https 问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -161,10 +161,10 @@ export default defineConfig({
     ],
     server: {
       host: true,
-      https: {
-        key: fs.readFileSync('./localhost+2-key.pem'),
-        cert: fs.readFileSync('./localhost+2.pem')
-      }
+      // https: {
+      //   key: fs.readFileSync('./localhost+2-key.pem'),
+      //   cert: fs.readFileSync('./localhost+2.pem')
+      // }
     }
   }
 })
