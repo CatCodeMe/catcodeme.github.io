530be4e84e8be4d3834ee9b6453c0ffbd898408c
catcodeme <1020082805@qq.com>
2025-08-20 19:35:17 +0800

    调整字体,调整 aside 配色

--- ec.config.mjs
+++ ec.config.mjs
@@ -37,7 +37,7 @@ export default defineEcConfig({
 
   /* Styles */
   styleOverrides: {
-    uiFontFamily: "'GuanKiapTsingKhai', 'Input Mono', 'Fira Code', 'monospace'",
+    uiFontFamily: "'JyunsaiKaai', 'Input Mono', 'Fira Code', 'monospace'",
     uiFontSize: '1em',
     codeBackground: (context) => (context.theme.name === 'vitesse-dark' ? '#0e0e0e' : '#fafafa'),
     codeFontFamily: "'Fira Code', 'monospace'",
