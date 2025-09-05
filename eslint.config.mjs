4439340baa471b3274275be43a9f298927b6ba54
catcodeme <1020082805@qq.com>
2025-09-05 17:43:51 +0800

    修复 build 错误

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,8 @@ import eslint from '@eslint/js'
 import eslintPluginAstro from 'eslint-plugin-astro'
 import tseslint from 'typescript-eslint'
 
-export default [
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const config = [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
@@ -13,3 +14,5 @@ export default [
     ignores: ['public/scripts/*', 'scripts/*', '.astro/', 'src/env.d.ts']
   }
 ]
+
+export default config
