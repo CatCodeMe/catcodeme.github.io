f636df545e5ec29fcbb6636b461241ab6d4da692
ccm <pingccm@gmail.com>
2026-08-19 17:47:36 +0800

    Remove message board page and navigation

--- src/site.config.ts
+++ src/site.config.ts
@@ -10,7 +10,6 @@ const menu: { title: string; link: string; icon?: IconName; iconImg?: string }[]
   // { title: 'Projects', link: '/projects' },
   { title: '关于', link: '/about', icon: 'about' },
   { title: '友链', link: '/links', icon: 'friendLink' },
-  { title: '留言板', link: '/msgboard', icon: 'msgboard' },
   { title: '', link: '/cats', iconImg: '/favicon/favicon-32x32.png' }
 ]
 
